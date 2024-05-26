1dfcf9450703134a226211e5c2c968c2c4da52da
Mibo <meebsawosika@gmail.com>
2024-05-26 00:43:23 -0600

    Created grid layout with equally sized squares
    Added logic to handle mouse entering element event

--- script.js
+++ script.js
@@ -3,5 +3,18 @@ const container = document.querySelector(".container")
 
 for(let i = 0; i<NUMBER_OF_GRIDS; i++){
     const div = document.createElement("div");
+    div.setAttribute("class","square");
     container.appendChild(div);
 }
+
+const squares = document.querySelectorAll("div .square");
+
+const changeColor = (square) => {
+    square.style.backgroundColor = "white";
+}
+
+squares.forEach((square) => {
+    square.addEventListener("mouseenter", () => {
+        square.classList.add("blacksquare")
+    })
+})
